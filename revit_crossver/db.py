573fbff24e9fc34aac80bde51279eb166b58cbfd
Konstantin <92411255+nodatasheet@users.noreply.github.com>
2023-07-26 13:55:34 +0800

    add validators and exceptions

--- revit_crossver/db.py
+++ revit_crossver/db.py
@@ -3,6 +3,7 @@ clr.AddReference("RevitAPI")
 from Autodesk.Revit import DB
 
 from abc import ABCMeta
+from exceptions import TypeValidationException
 
 _app = __revit__.Application  # noqa
 
@@ -27,9 +28,9 @@ class AbstractUnitType(object):
     pass
 
 
-class AbstractUnitsOfMeasurement(object):
+class AbstractUnitOfMeasure(object):
     """
-    Abstract units of measurement across Revit versions.
+    Abstract units of measure across Revit versions.
 
     since 2014: `DB.DisplayUnitType`\n
     since 2021: `DB.UnitTypeId`
@@ -37,15 +38,28 @@ class AbstractUnitsOfMeasurement(object):
 
 
 class _CrossVersionRevitObject(object):
+    """
+    Base class for utils that help to bypass changes in methods
+    across different versions of Revit API.
+    """
 
     __metaclass__ = ABCMeta
-
     _rvt_ver_num = RevitVersion.as_integer()
+
+    def _validate_type(self, obj, expected_type, err_msg=None):
+        # type: (object, type, str) -> None
+        if not isinstance(obj, expected_type):
+            raise TypeValidationException(
+                message=err_msg,
+                expected_type=expected_type,
+                provided_type=type(obj)
+            )
 
 
 class _Definition(_CrossVersionRevitObject):
     def __init__(self, definition):
         # type: (DB.Definition) -> None
+        self._validate_type(definition, DB.Definition)
         self._definition = definition
 
     def get_unit_type(self):
@@ -62,10 +76,11 @@ class _Definition(_CrossVersionRevitObject):
 class _Parameter(_CrossVersionRevitObject):
     def __init__(self, parameter):
         # type: (DB.Parameter) -> None
+        self._validate_type(parameter, DB.Parameter)
         self._parameter = parameter
 
     def get_units_of_measure(self):
-        # type: () -> AbstractUnitsOfMeasurement
+        # type: () -> AbstractUnitOfMeasure
         if self._rvt_ver_num < 2021:
             return self._parameter.DisplayUnitType
         return self._parameter.GetUnitTypeId()
@@ -74,10 +89,11 @@ class _Parameter(_CrossVersionRevitObject):
 class _FormatOptions(_CrossVersionRevitObject):
     def __init__(self, format_opts):
         # type: (DB.FormatOptions) -> None
+        self._validate_type(format_opts, DB.FormatOptions)
         self._format_opts = format_opts
 
     def get_units_of_measure(self):
-        # type: (DB.FormatOptions) -> AbstractUnitsOfMeasurement
+        # type: (DB.FormatOptions) -> AbstractUnitOfMeasure
         if self._rvt_ver_num < 2021:
             return self._format_opts.DisplayUnits
         return self._format_opts.GetUnitTypeId()
@@ -88,16 +104,17 @@ class _UnitUtils(_CrossVersionRevitObject):
         self._unit_utils = DB.UnitUtils
 
     def is_unit_of_measure(self, unit_of_measure):
-        # type: (AbstractUnitsOfMeasurement) -> bool
+        # type: (AbstractUnitOfMeasure) -> bool
         if self._rvt_ver_num < 2021:
             return self._unit_utils.IsValidDisplayUnit(unit_of_measure)
         return self._unit_utils.IsUnit(unit_of_measure)
 
     def is_valid_unit_of_measure(self, unit_type, unit_of_measure):
-        # type: (AbstractUnitType, AbstractUnitsOfMeasurement) -> bool
+        # type: (AbstractUnitType, AbstractUnitOfMeasure) -> bool
         if self._rvt_ver_num < 2021:
             return self._unit_utils.IsValidDisplayUnit(
-                unit_type, unit_of_measure
+                unit_type,
+                unit_of_measure
             )
         return self._unit_utils.IsValidUnit(unit_type, unit_of_measure)
 
